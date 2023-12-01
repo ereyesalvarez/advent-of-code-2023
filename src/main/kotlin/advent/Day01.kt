6bf65fd27619f68738d2babfce6a04038017995b
Eduardo Reyes <ereyesalvarez@gmail.com>
2023-12-01 08:48:28 +0100

    Advent 23 Reformat day 1

--- src/main/kotlin/advent/Day01.kt
+++ src/main/kotlin/advent/Day01.kt
@@ -4,33 +4,28 @@ import utils.mapStringToDigit
 
 class Day01 {
     fun execute01(input: String): Int {
-        val list = input.lines().map { s -> s.map { it.digitToIntOrNull(10) }.filterNotNull() }
-        return play01(list)
+        val mapped = input.lines().map { s -> s.map { it.digitToIntOrNull(10) }.filterNotNull() }
+        return play01(mapped)
     }
+
     fun execute02(input: String): Int {
         val mapped = input.lines().map { map02(it) }
         return play01(mapped)
     }
+
     private fun play01(list: List<List<Int>>): Int {
-        val sl = list.map {
-            val string = if (it.size > 1) {
-                "${it.first()}${it.last()}"
-            } else if (it.size == 1) {
-                "${it.first()}${it.first()}"
-            } else {
-                "0"
-            }
-            string
-        }
-        return sl.sumOf { it.toInt() }
+        return list
+            .map { "${it.first()}${it.last()}" }
+            .sumOf { it.toInt() }
     }
+
     private fun map02(input: String): List<Int> {
         val pattern = Regex("(?=(one|two|three|four|five|six|seven|eight|nine|\\d))")
         val matches = pattern.findAll(input)
         val groups = matches.map { g ->
-            g.groups.filterNotNull().filter { matchGroup ->
-                matchGroup.value != ""
-            }.map { it.value }.first()
+            g.groups.filterNotNull()
+                .map { it.value }
+                .first { it.isNotEmpty() }
         }.toList()
         return groups.map { g -> mapStringToDigit(g) }
     }
