6f042696e07b83d3ebb708b9fe8857d044e4dfac
Eduardo Reyes <ereyesalvarez@gmail.com>
2023-12-05 05:52:47 +0100

    Advent 23 Prepare Day 5

---
+++ src/main/kotlin/advent/Day05.kt
@@ -0,0 +1,20 @@
+package advent
+
+import kotlin.math.pow
+
+class Day05 {
+    fun execute01(input: String): Int {
+
+        return 0
+    }
+    fun execute02(input: String): Int {
+
+        return 0
+    }
+
+    private fun map(lines: List<String>): Any {
+        TODO()
+    }
+
+
+}
---
+++ src/test/kotlin/advent/Day05Test.kt
@@ -0,0 +1,44 @@
+package advent
+
+
+import utils.getFileAsText
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day05Test {
+    private val example = "data/05_ex.txt"
+    private val input = "data/05_in.txt"
+
+    private val service = Day05()
+
+    @Test
+    fun example1() {
+        val puzzleContent = getFileAsText(example)
+        val response = service.execute01(puzzleContent)
+        assertEquals(0, response)
+    }
+
+    @Test
+    fun exercise1() {
+        val puzzleContent = getFileAsText(input)
+        val response = service.execute01(puzzleContent)
+        println(response)
+        assertEquals(0, response)
+    }
+
+    @Test
+    fun example2() {
+        val puzzleContent = getFileAsText(example)
+        val response = service.execute02(puzzleContent)
+        assertEquals(0, response)
+    }
+
+    @Test
+    fun exercise2() {
+        val puzzleContent = getFileAsText(input)
+        val response = service.execute02(puzzleContent)
+        println(response)
+        assertEquals(0, response)
+    }
+
+}
