3b3b8bc24a843baceab317ff5af40440ddeb9bd3
Eduardo Reyes <ereyesalvarez@gmail.com>
2023-12-05 10:09:04 +0100

    Advent 23 Day 5 B

--- src/main/kotlin/advent/Day05.kt
+++ src/main/kotlin/advent/Day05.kt
@@ -4,6 +4,16 @@ import kotlin.math.pow
 import kotlin.time.Duration.Companion.seconds
 
 class Day05 {
+    private val steps = listOf(
+        Pair("seed", "soil"),
+        Pair("soil", "fertilizer"),
+        Pair("fertilizer", "water"),
+        Pair("water", "light"),
+        Pair("light", "temperature"),
+        Pair("temperature", "humidity"),
+        Pair("humidity", "location"),
+    )
+
     fun execute01(input: String): Long {
         val lines = input.lines()
         val seeds = mapSeeds(lines.first())
@@ -12,21 +22,96 @@ class Day05 {
 
         return result.minOfOrNull { it.second }!!
     }
+
     fun execute02(input: String): Long {
         val lines = input.lines()
         val seeds = mapSeedsSecond(lines.first())
         val map = mapSeedToSoil(lines)
-        println("SeedSize = ${seeds.size}")
         val result = seeds.map { mapSeedToLocation(it, map) }
         return result.minOfOrNull { it }!!
     }
 
-    private fun mapSeedToLocation(range: LongRange, map: Map<Pair<String, String>, List<GardenMap>>): Long{
-        println("map")
+    fun execute01B(input: String): Long {
+        val lines = input.lines()
+        val seeds = mapSeeds(lines.first()).map { it..it }
+        val map = mapTransformations(lines)
+        val result = transformToLocation(seeds, map)
+        return result.minOf { it.first }
+    }
+
+    fun execute02B(input: String): Long {
+        val lines = input.lines()
+        val seeds = mapSeedsSecond(lines.first())
+        val map = mapTransformations(lines)
+        val result = transformToLocation(seeds, map)
+        return result.minOf { it.first }
+    }
+
+    private fun transformToLocation(input: List<LongRange>, map: Map<Pair<String, String>, List<Pair<LongRange, Long>>>): List<LongRange> {
+        var partial = input
+        steps.forEach {
+            partial = calculateNext(partial, map[it]!!)
+        }
+        return partial
+    }
+
+    fun calculateNext(inputRange: List<LongRange>, transformations: List<Pair<LongRange, Long>>): List<LongRange> {
+        val result: MutableList<LongRange> = mutableListOf()
+        var rangesToExplore = inputRange
+        transformations.forEach { tr ->
+            val pendingRanges = mutableListOf<LongRange>()
+            rangesToExplore.forEach { c ->
+                val intersection = calculateIntersection(c, tr.first)
+                intersection.forEach {
+                    if (it.second){
+                        val newFrom = it.first.first + tr.second
+                        val newTo = it.first.last + tr.second
+                        result.add(newFrom..newTo)
+                    } else {
+                        pendingRanges.add(it.first)
+                    }
+                }
+            }
+            rangesToExplore = pendingRanges
+        }
+        if (rangesToExplore.isNotEmpty()){
+            result.addAll(rangesToExplore)
+        }
+        return result
+    }
+
+    fun calculateIntersection(a: LongRange, b: LongRange): List<Pair<LongRange, Boolean>> {
+        val result: MutableList<Pair<LongRange, Boolean>> = mutableListOf()
+        if (a.first < b.first && a.last < b.first){
+            return listOf(Pair(a, false))
+        }
+        if (a.first > b.last){
+            return listOf(Pair(a, false))
+        }
+        if (a.first >= b.first && a.last <= b.last){
+            return listOf(Pair(a, true))
+        }
+        if (a.first < b.first){
+            result.add(Pair(a.first..<b.first, false))
+            return if (a.last < b.last){
+                result.add(Pair(b.first..a.last, true))
+                result
+            } else {
+                result.add(Pair(b.first..b.last, true))
+                result.add(Pair(b.last +1 .. a.last, false))
+                result
+            }
+        }
+        result.add(Pair(a.first..b.last, true))
+        result.add(Pair(b.last+1..a.last, false))
+        return result
+    }
+
+    private fun mapSeedToLocation(range: LongRange, map: Map<Pair<String, String>, List<GardenMap>>): Long {
         var minimum = Long.MAX_VALUE
         range.forEach {
-           val result =  mapSeedToLocation(it, map)
-            if (result.second < minimum){
+            val result = mapSeedToLocation(it, map)
+            if (result.second < minimum) {
                 minimum = result.second
             }
         }
@@ -34,30 +119,21 @@ class Day05 {
     }
 
 
-        private fun mapSeedToLocation(input: Long, map: Map<Pair<String, String>, List<GardenMap>>): Pair<Long, Long>{
-        val transforms = listOf(
-            Pair("seed", "soil"),
-            Pair("soil", "fertilizer"),
-            Pair("fertilizer", "water"),
-            Pair("water", "light"),
-            Pair("light", "temperature"),
-            Pair("temperature", "humidity"),
-            Pair("humidity", "location"),
-        )
+    private fun mapSeedToLocation(input: Long, map: Map<Pair<String, String>, List<GardenMap>>): Pair<Long, Long> {
         var current = input
-        transforms.forEach {
+        steps.forEach {
             current = mapToNext(current, map, it)
         }
         return Pair(input, current)
     }
 
-    private fun mapToNext(input: Long, map: Map<Pair<String, String>, List<GardenMap>>, x: Pair<String, String>): Long{
+    private fun mapToNext(input: Long, map: Map<Pair<String, String>, List<GardenMap>>, x: Pair<String, String>): Long {
         val currentMap = map[x]!!
         val find = currentMap.filter { isInRange(input, it) }
-        if (find.size > 1){
+        if (find.size > 1) {
             throw Exception("Error")
         }
-        return if (find.isEmpty()){
+        return if (find.isEmpty()) {
             input
         } else {
             val n = find.first()
@@ -65,23 +141,22 @@ class Day05 {
         }
     }
 
-    private fun isInRange(value: Long, x: GardenMap): Boolean{
-        val range = (x.orig..<x.orig+x.count)
+    private fun isInRange(value: Long, x: GardenMap): Boolean {
+        val range = (x.orig..<x.orig + x.count)
         return range.contains(value)
     }
 
-    private fun mapSeeds(line: String): List<Long>{
+    private fun mapSeeds(line: String): List<Long> {
         return line.split("seeds: ").last().split(" ").map { it.trim().toLong() }
     }
 
 
-    private fun mapSeedsSecond(line: String): List<LongRange>{
-        val numbers =  line.split("seeds: ").last().split(" ").map { it.trim().toLong() }
-        val rangeI = numbers.indices
+    private fun mapSeedsSecond(line: String): List<LongRange> {
+        val numbers = line.split("seeds: ").last().split(" ").map { it.trim().toLong() }
         var i = 0
         val ranges: MutableList<LongRange> = mutableListOf()
-        while (i < numbers.size){
-            ranges.add(numbers[i]..< numbers[i] + numbers[i+1])
+        while (i < numbers.size) {
+            ranges.add(numbers[i]..<numbers[i] + numbers[i + 1])
             i++
             i++
         }
@@ -89,11 +164,11 @@ class Day05 {
     }
 
     private fun mapSeedToSoil(lines: List<String>): MutableMap<Pair<String, String>, List<GardenMap>> {
-        val result: MutableMap<Pair<String,String>, List<GardenMap>> = mutableMapOf()
+        val result: MutableMap<Pair<String, String>, List<GardenMap>> = mutableMapOf()
         var mapping = false
         var currentMap: Pair<String, String> = Pair("", "")
         var gardenList = mutableListOf<GardenMap>()
-        lines.forEach {l ->
+        lines.forEach { l ->
             if (l.contains(" map:")) {
                 mapping = true
                 val data = l.split(" map:").first().split("-")
@@ -110,6 +185,34 @@ class Day05 {
             }
         }
         return result
+    }
+
+
+    private fun mapTransformations(lines: List<String>): MutableMap<Pair<String, String>, List<Pair<LongRange, Long>>> {
+        var mapping = false
+        var currentMap: Pair<String, String> = Pair("", "")
+        var transformations = mutableListOf<Pair<LongRange, Long>>()
+        val transformationsMap: MutableMap<Pair<String, String>, List<Pair<LongRange, Long>>> = mutableMapOf()
+        lines.forEach { l ->
+            if (l.contains(" map:")) {
+                mapping = true
+                val data = l.split(" map:").first().split("-")
+                currentMap = Pair(data.first(), data.last())
+            } else if (mapping) {
+                if (l.isBlank()) {
+                    mapping = false
+                    transformationsMap[currentMap] = transformations
+                    transformations = mutableListOf()
+                } else {
+                    val x = l.split(" ")
+                    val dest = x[0].toLong()
+                    val orig = x[1].toLong()
+                    val count = x[2].toLong()
+                    transformations.add(Pair(orig..<orig + count, dest - orig))
+                }
+            }
+        }
+        return transformationsMap
     }
 
     data class GardenMap(val dest: Long, val orig: Long, val count: Long)
--- src/test/kotlin/advent/Day05Test.kt
+++ src/test/kotlin/advent/Day05Test.kt
@@ -1,8 +1,10 @@
 package advent
 
 
+import org.junit.jupiter.api.Disabled
 import utils.getFileAsText
 import kotlin.test.Test
+import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 class Day05Test {
@@ -27,6 +29,7 @@ class Day05Test {
     }
 
     @Test
+    @Disabled
     fun example2() {
         val puzzleContent = getFileAsText(example)
         val response = service.execute02(puzzleContent)
@@ -34,6 +37,7 @@ class Day05Test {
     }
 
     @Test
+    @Disabled
     fun exercise2() {
         val puzzleContent = getFileAsText(input)
         val response = service.execute02(puzzleContent)
@@ -41,4 +45,54 @@ class Day05Test {
         assertEquals(26714516, response)
     }
 
+    @Test
+    fun example1b() {
+        val puzzleContent = getFileAsText(example)
+        val response = service.execute01B(puzzleContent)
+        assertEquals(35, response)
+    }
+
+    @Test
+    fun example2b() {
+        val puzzleContent = getFileAsText(example)
+        val response = service.execute02B(puzzleContent)
+        assertEquals(46, response)
+    }
+
+    @Test
+    fun exercise2b() {
+        val puzzleContent = getFileAsText(input)
+        val response = service.execute02B(puzzleContent)
+        println(response)
+        assertEquals(26714516, response)
+    }
+
+    @Test
+    fun calculateNext(){
+        val data = listOf(
+            Pair(2L..3L, 10L),
+            Pair(5L..5L, 100L)
+        )
+        val input = listOf(1L..10)
+        val result = service.calculateNext(input, data).sortedBy { it.first }
+        val expected = listOf(1L..1, 4L..4, 6L..10, 12L..13, 105L..105)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun calculateIntersection(){
+        val a = 79L..79
+        val b = 59L..97
+        val result = service.calculateIntersection(a, b)
+        val expect = listOf(Pair(79..79L, true))
+        assertEquals(expect, result)
+    }
+    @Test
+    fun calculateIntersection2(){
+        val a = 1L..10
+        val b = 11L..100
+        val result = service.calculateIntersection(a, b)
+        val expect = listOf(Pair(1..10L, false))
+        assertEquals(expect, result)
+    }
 }
