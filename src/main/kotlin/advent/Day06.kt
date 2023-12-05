4b4c896785ca1d82f9ddb9922ef5c6ab13b4f805
Eduardo Reyes <ereyesalvarez@gmail.com>
2023-12-06 00:40:01 +0100

    Advent 23 Prepare Day 6

---
+++ src/main/kotlin/advent/Day06.kt
@@ -0,0 +1,15 @@
+package advent
+
+class Day06 {
+    fun execute01(input: String): Long {
+        val lines = input.lines()
+        return 0
+    }
+
+    fun execute02(input: String): Long {
+        val lines = input.lines()
+        return 0
+    }
+
+
+}
