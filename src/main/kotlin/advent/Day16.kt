dcddf1215bebe009c47b8367880ca0419d342d64
Eduardo Reyes <ereyesalvarez@gmail.com>
2023-12-17 23:44:57 +0100

    Advent 23 Add days

---
+++ src/main/kotlin/advent/Day16.kt
@@ -0,0 +1,10 @@
+package advent
+
+class Day16 {
+    fun execute01(input: String): Long {
+        return 0
+    }
+    fun execute02(input: String): Long {
+        return 0
+    }
+}
---
+++ src/main/kotlin/advent/Day17.kt
@@ -0,0 +1,10 @@
+package advent
+
+class Day17 {
+    fun execute01(input: String): Long {
+        return 0
+    }
+    fun execute02(input: String): Long {
+        return 0
+    }
+}
